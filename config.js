2f1d98e7a779110304d3fe783bc59f358ddaee82
tanapolza42 <firsttanapol@outlook.com>
2021-04-02 20:20:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID':  '5140508529352916', // your App ID
+		'clientSecret':  '0c59dd8af87ce9cc27c0324e4ffd9034', // your App Secret
+		'callbackURL':  'http://localhost:3000/auth/facebook/callback'
+	}
+}
